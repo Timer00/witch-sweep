6e5600c884bb8bf94a290d01fe749f0055e1c274
Théo Carrara <theocarraraleao@gmail.com>
2023-11-05 17:59:06 +0100

    Add dyslexic font & title component

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,7 @@ import cleaning from './videos/cleaning.mov';
 import homework from './videos/homework.mov';
 import zoom from './videos/zoom.mov';
 import room from './videos/room.mov';
+import logo from './images/logo.png'
 
-export {castleLoop, castleIntro, cleaning, homework, zoom, room };
+export { castleLoop, castleIntro, cleaning, homework, zoom, room, logo };
 
---
+++ src/components/Title.tsx
@@ -0,0 +1,13 @@
+import { type ReactNode } from "react";
+
+interface TitleProps {
+  children: ReactNode;
+}
+
+const Title = ({ children }: TitleProps) =>
+  <p id={'gameTitle'}
+     className="font-dyslexic text-yellow-300 md:text-4xl lg:text-6xl">
+    {children}
+  </p>
+
+export default Title;
