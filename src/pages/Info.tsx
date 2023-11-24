a7271e68211c790c3b7b387e792cd91b7e75ef9c
muricodes <151963863+muricodes@users.noreply.github.com>
2023-11-24 23:14:12 +0100

    Update Info.tsx

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -51,27 +51,27 @@ const faqs = [
   {
     question: "WOZU NUTZE ICH „HOCUSFOCUS“?",
     answer:
-      "Ihr Kind mit ADHS soll durch soziales Feedback, Wettbewerb und Belohnung dazu motiviert werden, sich alltäglichen Herausforderungen zu stellen, speziell dem Hausaufgaben machen und dem Aufräumen. Beide Aufgaben verwenden Timer mit unterschiedlichen Funktionen. Beim Aufräumen muss das Kind schneller sein als die Hexe. Beim Hausaufgaben machen jedoch sollte das Kind länger durchhalten als die Hexe. Je nach Erfolg und Zeit wird das Kind mit einer oder zwei Münzen belohnt.",
+      "Ihr Kind mit ADHS soll durch soziales Feedback, Wettbewerb und Belohnung dazu motiviert werden, alltägliche Herausforderungen zu bewältigen, insbesondere das Erledigen der Hausaufgaben und das Aufräumen. Für beide Aufgaben gibt es Timer mit unterschiedlichen Funktionen. Während das Kind beim Aufräumen schneller als die Hexe sein muss, soll es beim Hausaufgaben machen länger durchhalten als die Hexe. Abhängig von Erfolg und Zeit erhält das Kind eine oder zwei Münzen als Belohnung.",
   },
   {
     question: "WAS SIND DIE MÜNZEN?",
     answer:
-      "Die Münzen dienen primär als kurzfristige Belohnungen für Ihr Kind. Sie helfen, ein positives Gefühl mit der Aufgabenerfüllung zu verbinden. Auf lange Sicht sollen die Münzen aber auch zu größeren Belohnungen führen. Ihre Mitarbeit ist hierbei gefragt, da die Münzen eine Art Punktesammlung darstellen, die Sie in fassbare Belohnungen umsetzen sollten.",
+      "Die Münzen sind kurzfristige Belohnungen für Ihr Kind, die es motivieren sollen, seine Aufgaben zu erledigen. So verknüpft es die Erfüllung von Aufgaben mit einem positiven Gefühl. Die Münzen sollten langfristig aber zu größeren Belohnungen führen. Hierfür ist Ihre Mitarbeit gefragt, da die Münzen gleichzeitig als Punktesystem dienen, das in fassbare Belohnungen umgesetzt werden sollte.",
   },
   {
     question: "WIE BELOHNE ICH MEIN KIND?",
     answer:
-      "Erstellen Sie gemeinsam mit Ihrem Kind einen Vertrag, in dem Sie festlegen, welche Belohnungen es für eine bestimmte Anzahl gesammelter Münzen gibt. Kurzfristige Belohnungen könnten für 1-3 Münzen ein Sticker oder eine Kleinigkeit sein. Für eine größere Menge an Münzen könnten Belohnungen wie ein Kinobesuch, ein neues Paar Schuhe oder zusätzliche Spielzeit vereinbart werden. Stellen Sie sicher, dass die Belohnungen nicht nur materieller Natur sind. Achten Sie bei der Belohnung mit Süßigkeiten auf Moderation, um ungesunde Gewohnheiten zu vermeiden.",
+      "Erstellen Sie gemeinsam mit Ihrem Kind einen Vertrag, in dem Sie festlegen, welche Belohnungen es für eine bestimmte Anzahl gesammelter Münzen gibt. Kurzfristige Belohnungen könnten für 1-3 Münzen ein Sticker oder eine Kleinigkeit sein. Für eine größere Anzahl an Münzen könnten Belohnungen wie ein Kinobesuch, ein neues Paar Schuhe oder zusätzliche Spielzeit vereinbart werden. Stellen Sie sicher, dass die Belohnungen nicht nur materieller Natur sind. Achten Sie vor allem bei der Belohnung mit Süßigkeiten auf Moderation, um ungesunde Gewohnheiten zu vermeiden.",
   },
   {
     question: "WANN SOLLTE ICH MEIN KIND BELOHNEN?",
     answer:
-      "Es ist wichtig, Belohnungen zeitnah nach Erreichen eines Ziels auszuhändigen. Bei jüngeren oder besonders beeinträchtigten Kindern sollte der Tausch der Münzen gegen Belohnungen fast unmittelbar erfolgen. Ältere Kinder können eventuell ein bis zwei Tage auf ihre Belohnung warten. Dennoch ist für Kinder mit ADHS die schnelle positive Rückmeldung besonders wertvoll und wichtig für die Verstärkende Wirkung der Belohnung.",
+      "Es ist wichtig, Belohnungen zeitnah nach Erreichen eines Ziels auszuhändigen. Bei jüngeren oder besonders beeinträchtigten Kindern sollte der Tausch der Münzen gegen Belohnungen fast unmittelbar erfolgen. Ältere Kinder können eventuell ein bis zwei Tage auf ihre Belohnung warten. Dennoch ist für Kinder mit ADHS die schnelle, positive Rückmeldung besonders wertvoll und wichtig für die verstärkende Wirkung der Belohnung.",
   },
   {
     question: "WAS GIBT ES NOCH ZU BEACHTEN?",
     answer:
-      "Es ist wichtig, dass Ihr Kind das Belohnungssystem versteht und dass es ansprechend ist. Bieten Sie verschiedene Belohnungsoptionen an, um das Interesse zu erhalten. Prüfen Sie die Belohnungen auf Praktikabilität, damit keine falschen Versprechen entstehen! Konsequenz ist der Schlüssel: Belohnen Sie stets, selbst wenn Sie müde oder gestresst sind. Achten Sie immer auf eine gute Beziehung zu Ihrem Kind; die Bindung zwischen Ihnen beiden ist essenziell.",
+      "Es ist wichtig, dass Ihr Kind das Belohnungssystem versteht und dass es ansprechend ist. Bieten Sie verschiedene Belohnungsoptionen an, um das Interesse aufrechtzuerhalten. Prüfen Sie die Belohnungen immer auf Praktikabilität, damit keine falschen Versprechen entstehen! Konsequenz ist der Schlüssel: Belohnen Sie stets, selbst wenn Sie müde oder gestresst sind und achten Sie stets auf eine gute Beziehung zu Ihrem Kind; die Bindung zwischen Ihnen beiden ist unerlässlich.",
   },
 ];
 
