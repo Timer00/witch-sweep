275d674feea4520a73a7c769e57965eae2b065a3
Timer00 <theocarraraleao@gmail.com>
2024-12-28 14:29:07 +0100

    Shrink legal stuff button

--- src/assets/icons/LegalIcon.tsx
+++
@@ -1,3 +0,0 @@
-import { Scale } from 'lucide-react';
-
-export const LegalIcon = () => <Scale size={24} />;
--- src/pages/LegalInfo.tsx
+++ src/pages/LegalInfo.tsx
@@ -1,35 +1,49 @@
-import { LegalIcon } from "@/assets/icons/LegalIcon";
 import FullScreen from "@/components/FullScreen";
+import { Scale } from "lucide-react";
 
 interface LegalInfoButtonProps {
   pageIndex: number;
   onClick: () => void;
 }
 
-export const LegalInfoButton = ({ pageIndex, onClick }: LegalInfoButtonProps) => (
-  <button onClick={onClick} className="z-2 absolute left-4 top-[-16px] m-4">
-    <div className="flex items-center space-x-1 p-2 text-white underline underline-offset-[5px]">
-      { pageIndex === 0 &&
-          <span className="text-xs font-semibold text-white">
-            {"Impressum & Datenschutz "}
-          </span>
-      }
-      <span className="text-lg font-semibold text-white"><LegalIcon/></span>
-    </div>
-  </button>
-);
+export const LegalInfoButton = ({
+  pageIndex,
+  onClick,
+}: LegalInfoButtonProps) => {
+  if (pageIndex !== 0) {
+    return null;
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="z-2 absolute bottom-[-16px] left-32 m-4"
+    >
+      <div className="flex items-center space-x-1 p-2 text-white underline underline-offset-[5px]">
+        <span className="text-[0.5rem] font-semibold text-white">
+          {"Impressum & Datenschutz "}
+        </span>
+        <Scale size={12} />
+      </div>
+    </button>
+  );
+};
 
 const legalInfo = [
   {
     title: "IMPRESSUM",
     content: (
-      <div className="prose prose-headings:font-bold prose-headings:mb-2 prose-p:mb-4">
-        <p className="font-bold mb-2">HocusFocus</p>
+      <div className="prose prose-headings:mb-2 prose-headings:font-bold prose-p:mb-4">
+        <p className="mb-2 font-bold">HocusFocus</p>
         <p>
-          Muriel Antoun<br/>
-          Bleichstraße 48<br/>
-          17489 Greifswald<br/>
-          muriel.antoun@stud.uni-greifswald.de<br/>
+          Muriel Antoun
+          <br />
+          Bleichstraße 48
+          <br />
+          17489 Greifswald
+          <br />
+          muriel.antoun@stud.uni-greifswald.de
+          <br />
           +49 176 55598461
         </p>
         <p className="mt-4">
@@ -41,25 +55,59 @@ const legalInfo = [
   {
     title: "DATENSCHUTZERKLÄRUNG",
     content: (
-      <div className="prose prose-headings:font-bold prose-headings:mb-2 prose-p:mb-4">
-        <p><strong className="font-bold">Verantwortlicher</strong><br/>
-        Muriel Antoun<br/>
-        muriel.antoun@stud.uni-greifswald.de</p>
+      <div className="prose prose-headings:mb-2 prose-headings:font-bold prose-p:mb-4">
+        <p>
+          <strong className="font-bold">Verantwortlicher</strong>
+          <br />
+          Muriel Antoun
+          <br />
+          muriel.antoun@stud.uni-greifswald.de
+        </p>
 
-        <p><strong className="font-bold">Erhobene Daten</strong><br/>
-        Diese Website speichert keine personenbezogenen Daten auf Servern. Der Benutzername wird lokal im Browser des Nutzers in der Local Storage gespeichert. Es erfolgt keine Übertragung dieser Daten an unsere Server.</p>
+        <p>
+          <strong className="font-bold">Erhobene Daten</strong>
+          <br />
+          Diese Website speichert keine personenbezogenen Daten auf Servern. Der
+          Benutzername wird lokal im Browser des Nutzers in der Local Storage
+          gespeichert. Es erfolgt keine Übertragung dieser Daten an unsere
+          Server.
+        </p>
 
-        <p><strong className="font-bold">Rechte der Nutzer</strong><br/>
-        Benutzer haben das Recht, gespeicherte Daten im Browser selbstständig zu löschen. Da keine Daten an uns übermittelt werden, können wir keine darüber hinausgehenden Rechte wie Löschung oder Berichtigung von Daten auf unseren Systemen anbieten.</p>
+        <p>
+          <strong className="font-bold">Rechte der Nutzer</strong>
+          <br />
+          Benutzer haben das Recht, gespeicherte Daten im Browser selbstständig
+          zu löschen. Da keine Daten an uns übermittelt werden, können wir keine
+          darüber hinausgehenden Rechte wie Löschung oder Berichtigung von Daten
+          auf unseren Systemen anbieten.
+        </p>
 
-        <p><strong className="font-bold">Keine Verwendung von Cookies oder Tracking</strong><br/>
-        Unsere Website verwendet keine Cookies, Tracking-Tools oder Analysetools.</p>
+        <p>
+          <strong className="font-bold">
+            Keine Verwendung von Cookies oder Tracking
+          </strong>
+          <br />
+          Unsere Website verwendet keine Cookies, Tracking-Tools oder
+          Analysetools.
+        </p>
 
-        <p><strong className="font-bold">Rechtsgrundlage der Datenverarbeitung</strong><br/>
-        Die lokale Speicherung des Benutzernamens erfolgt ausschließlich zur Nutzung der Website und basiert auf der Zustimmung des Nutzers durch die aktive Eingabe.</p>
+        <p>
+          <strong className="font-bold">
+            Rechtsgrundlage der Datenverarbeitung
+          </strong>
+          <br />
+          Die lokale Speicherung des Benutzernamens erfolgt ausschließlich zur
+          Nutzung der Website und basiert auf der Zustimmung des Nutzers durch
+          die aktive Eingabe.
+        </p>
 
-        <p><strong className="font-bold">Hosting durch Vercel</strong><br/>
-        Diese Website wird von Vercel gehostet. Möglicherweise werden durch Vercel Daten wie IP-Adressen oder Zugriffsprotokolle gespeichert. Details dazu entnehmen Sie bitte der Datenschutzerklärung von Vercel.</p>
+        <p>
+          <strong className="font-bold">Hosting durch Vercel</strong>
+          <br />
+          Diese Website wird von Vercel gehostet. Möglicherweise werden durch
+          Vercel Daten wie IP-Adressen oder Zugriffsprotokolle gespeichert.
+          Details dazu entnehmen Sie bitte der Datenschutzerklärung von Vercel.
+        </p>
       </div>
     ),
   },
@@ -72,16 +120,14 @@ interface LegalInfoProps {
 const LegalInfo = ({ onClose }: LegalInfoProps) => {
   return (
     <FullScreen onClose={onClose}>
-      <div className="px-8 w-full h-full overflow-y-scroll">
-        <h1 className="mb-4 text-2xl font-bold center">
+      <div className="h-full w-full overflow-y-scroll px-8">
+        <h1 className="center mb-4 text-2xl font-bold">
           RECHTLICHE INFORMATIONEN
         </h1>
         {legalInfo.map((section, index) => (
           <div key={index} className="mb-6">
-            <h2 className="text-xl font-bold mb-2">{section.title}</h2>
-            <div className="bg-amber-100 p-4 rounded-md">
-              {section.content}
-            </div>
+            <h2 className="mb-2 text-xl font-bold">{section.title}</h2>
+            <div className="rounded-md bg-amber-100 p-4">{section.content}</div>
           </div>
         ))}
       </div>
